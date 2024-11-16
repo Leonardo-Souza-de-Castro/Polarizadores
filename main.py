8aed4dd611715b53ff716a38bd8b281a6caa304a
Leonardo <leonardo030605@gmail.com>
2024-11-16 19:07:58 -0300

    funções finalizadas

--- main.py
+++ main.py
@@ -140,10 +140,10 @@ n - Não
 """)
             
             if tipo == "s":
-                I1 = Intensidade/(cos(angulo1)**2)
-                I2 = I1/(cos(angulo2)**2)
+                I1 = Intensidade*(cos(angulo1)**2)
+                I2 = I1*(cos(angulo2)**2)
 
-                I3 = I2/(cos(angulo3)**2)
+                I3 = I2*(cos(angulo3)**2)
             else:
 
                 I1 = Intensidade/2
@@ -165,12 +165,70 @@ I3 = {I3}
             angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
             angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
             angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
+
+            tipo = input("""
+A luz inicial é polarizada?
+                  
+s - Sim
+n - Não
+
+""")
+            
+            if tipo == "s":
+                I0 = Intensidade/(cos(angulo1)**2)
+                I2 = Intensidade*(cos(angulo2)**2)
+
+                I3 = I2*(cos(angulo3)**2)
+            else:
+
+                I0 = Intensidade*2
+                I2 = Intensidade*(cos(angulo2)**2)
+                I3 = I2*(cos(angulo3)**2)
+
+                
+
+            print(f"""
+I0 = {I0}
+
+I2 = {I2}                  
+
+I3 = {I3}
+""")
         case(3):
             # Nesse caso entra os angulos a intensidade I2 e sai as instensidade I0, I1 e I3
             Intensidade = float(input("Digite a intensidade de luz (I2): "))
             angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
             angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
             angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
+
+            tipo = input("""
+A luz inicial é polarizada?
+                  
+s - Sim
+n - Não
+
+""")
+            
+            if tipo == "s":
+                I1 = Intensidade/(cos(angulo2)**2)
+                I0 = I1/(cos(angulo1)**2)
+
+                I3 = Intensidade*(cos(angulo3)**2)
+            else:
+
+                I1 = Intensidade/(cos(angulo2)**2)
+                I0 = I1*2
+                I3 = Intensidade*(cos(angulo3)**2)
+
+                
+
+            print(f"""
+I0 = {I0}
+
+I1 = {I1}                  
+
+I3 = {I3}
+""")
         case(4):
             # Nesse caso entra os angulos a intensidade I3 e sai as instensidade I0, I1 e I2
             Intensidade = float(input("Digite a intensidade de luz (I3): "))
