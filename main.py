43fa4e99a9178fb37236ebfc73a4b0974d1fd6df
Leonardo <leonardo030605@gmail.com>
2024-11-16 21:45:53 -0300

    funções finalizadas, falta o paragrafo e mais alguns testes

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from math import cos, radians
+from decimal import Decimal
+
+print("""
+_____________________________________________________
+|                   Autores:                        |
+|                                                   |
+|   Bianca Silva Oliveira - R.A: 22.123.113-7       |
+|   Felipe Brum Pereira - R.A: 22.123.012-9         |
+|   Leonardo Souza de Castro - R.A: 22.123.114-5    |
+|___________________________________________________|
+""")
 
 resposta = int(input("""
 Digite qual opção você gostaria de seguir:
@@ -6,10 +17,12 @@ Digite qual opção você gostaria de seguir:
 1 - Calculos com 2 polarizadores
 
 2 - Calculo com 3 polarizadores
-"""))
 
-if resposta == 1:
-    opcao = int(input("""
+0 - Sair
+"""))
+while resposta != 0 :
+    if resposta == 1:
+        opcao = int(input("""
 Digite qual opção gostaria de seguir:
     
 1 - θ1, θ2, I0
@@ -20,14 +33,14 @@ Digite qual opção gostaria de seguir:
 
 """))
 
-    match opcao:
-        case(1):
-            # Nesse caso entra os angulos a intensidade I0 e sai as instensidade I1 e I2
-            Intensidade = float(input("Digite a intensidade de luz (I0): "))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
+        match opcao:
+            case(1):
+                # Nesse caso entra os angulos a intensidade I0 e sai as instensidade I1 e I2
+                Intensidade = Decimal(input("Digite a intensidade de luz (I0) W/cm²: "))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
 
-            tipo = input("""
+                tipo = input("""
 A luz inicial é polarizada?
                   
 s - Sim
@@ -35,83 +48,117 @@ n - Não
 
 """)
             
-            if tipo == "s":
-                I1 = Intensidade*(cos(angulo1)**2)
+                if tipo == "s":
+                    I1 = Decimal(Intensidade*Decimal(cos(angulo1)**2))
 
-                I2 = I1*(cos(angulo2)**2)
-            else:
-                I1 = Intensidade/2
-                print(cos(angulo2))
-                print(cos(angulo2)**2)
+                    I2 = Decimal(I1*Decimal(cos(angulo2)**2))
+                else:
+                    I1 = Decimal(Intensidade/2)
 
-                I2 = I1*(cos(angulo2)**2)
+                    I2 = Decimal(I1*Decimal(cos(angulo2)**2))
 
-            
-            print(f"""
-I1 = {I1}
-
-I2 = {I2}
-""")
-
-        case(2):
-            # Nesse caso entra os angulos a intensidade I1 e sai as instensidade I0 e I2
-            Intensidade = float(input("Digite a intensidade de luz (I1): "))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-
-            tipo = input("""
-A luz inicial é polarizada?
-                  
-s - Sim
-n - Não
-
-""")
-            
-            if tipo == "s":
-                I0 = Intensidade/(cos(angulo1)**2)
-
-                I2 = Intensidade*(cos(angulo2)**2)
-            else:
-                I0 = Intensidade*2
-
-                I2 = Intensidade*(cos(angulo2)**2)
-
-            print(f"""
-I0 = {I0}
-
-I2 = {I2}
-""")
-        case(3):
-            # Nesse caso entra os angulos a intensidade I2 e sai as instensidade I0 e I1
-            Intensidade = float(input("Digite a intensidade de luz (I2): "))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-
-            tipo = input("""
-A luz inicial é polarizada?
-                  
-s - Sim
-n - Não
-
-""")
-            
-            if tipo == "s":
-                I1 = Intensidade/(cos(angulo2)**2)
-
-                I0 = I1/(cos(angulo1)**2)
-            else:
-                I1 = Intensidade/(cos(angulo2)**2)
                 
-                I0 = I1*2
+                print(f"""
+Resultados:
 
-            print(f"""
-I0 = {I0}
+I1 = {I1:.2e} W/cm²
 
-I1 = {I1}
+I2 = {I2:.2e} W/cm²
 """)
 
-else:
-    opcao = int(input("""
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))
+
+            case(2):
+                # Nesse caso entra os angulos a intensidade I1 e sai as instensidade I0 e I2
+                Intensidade = Decimal(input("Digite a intensidade de luz (I1) W/cm²: "))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+
+                tipo = input("""
+A luz inicial é polarizada?
+                  
+s - Sim
+n - Não
+
+""")
+                
+                if tipo == "s":
+                    I0 = Decimal(Intensidade/Decimal(cos(angulo1)**2))
+
+                    I2 = Decimal(Intensidade*Decimal(cos(angulo2)**2))
+                else:
+                    I0 = Decimal(Intensidade*2)
+
+                    I2 = Decimal(Intensidade*Decimal(cos(angulo2)**2))
+
+                print(f"""
+Resultados:
+
+I0 = {I0:.2e} W/cm²
+
+I2 = {I2:.2e} W/cm²
+""")
+
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))
+            case(3):
+                # Nesse caso entra os angulos a intensidade I2 e sai as instensidade I0 e I1
+                Intensidade = Decimal(input("Digite a intensidade de luz (I2) W/cm²: "))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+
+                tipo = input("""
+A luz inicial é polarizada?
+                  
+s - Sim
+n - Não
+
+""")
+            
+                if tipo == "s":
+                    I1 = Decimal(Intensidade/Decimal(cos(angulo2)**2))
+
+                    I0 = Decimal(I1/Decimal(cos(angulo1)**2))
+                else:
+                    I1 = Decimal(Intensidade/Decimal(cos(angulo2)**2))
+                    
+                    I0 = Decimal(I1*2)
+
+                print(f"""
+Resultados:
+
+I0 = {I0:.2e} W/cm²
+
+I1 = {I1:.2e} W/cm²
+""")
+
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))
+
+    else:
+        opcao = int(input("""
 Digite qual opção gostaria de seguir:
     
 1 - θ1, θ2, θ3, I0
@@ -123,15 +170,15 @@ Digite qual opção gostaria de seguir:
 4 - θ1, θ2, θ3, I3
 """))
 
-    match opcao:
-        case(1):
-            # Nesse caso entra os angulos a intensidade I0 e sai as instensidade I1, I2 e I3
-            Intensidade = float(input("Digite a intensidade de luz (I0): "))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-            angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
+        match opcao:
+            case(1):
+                # Nesse caso entra os angulos a intensidade I0 e sai as instensidade I1, I2 e I3
+                Intensidade = Decimal(input("Digite a intensidade de luz (I0) W/cm²: "))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+                angulo3 = radians(float(input("Digite o angulo do terceiro polarizador (θ3°): ")))
 
-            tipo = input("""
+                tipo = input("""
 A luz inicial é polarizada?
                   
 s - Sim
@@ -139,34 +186,44 @@ n - Não
 
 """)
             
-            if tipo == "s":
-                I1 = Intensidade*(cos(angulo1)**2)
-                I2 = I1*(cos(angulo2)**2)
+                if tipo == "s":
+                    I1 = Decimal(Intensidade*Decimal(cos(angulo1)**2))
+                    I2 = Decimal(I1*Decimal(cos(angulo2)**2))
 
-                I3 = I2*(cos(angulo3)**2)
-            else:
+                    I3 = Decimal(I2*Decimal(cos(angulo3)**2))
+                else:
 
-                I1 = Intensidade/2
-                I2 = I1*(cos(angulo2)**2)
-                I3 = I2*(cos(angulo3)**2)
+                    I1 = Decimal(Intensidade/2)
+                    I2 = Decimal(I1*Decimal(cos(angulo2)**2))
+                    I3 = Decimal(I2*Decimal(cos(angulo3)**2))
+                print(f"""
+Resultados:
 
-                
+I1 = {I1:.2e} W/cm²
 
-            print(f"""
-I1 = {I1}
+I2 = {I2:.2e} W/cm²                  
 
-I2 = {I2}                  
-
-I3 = {I3}
+I3 = {I3:.2e} W/cm²
 """)
-        case(2):
-            # Nesse caso entra os angulos a intensidade I1 e sai as instensidade I0, I2 e I3
-            Intensidade = float(input("Digite a intensidade de luz (I1): "))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-            angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
 
-            tipo = input("""
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))                
+
+            case(2):
+                # Nesse caso entra os angulos a intensidade I1 e sai as instensidade I0, I2 e I3
+                Intensidade = Decimal(input("Digite a intensidade de luz (I1) W/cm²: "))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+                angulo3 = radians(float(input("Digite o angulo do terceiro polarizador (θ3°): ")))
+
+                tipo = input("""
 A luz inicial é polarizada?
                   
 s - Sim
@@ -174,93 +231,124 @@ n - Não
 
 """)
             
-            if tipo == "s":
-                I0 = Intensidade/(cos(angulo1)**2)
-                I2 = Intensidade*(cos(angulo2)**2)
+                if tipo == "s":
+                    I0 = Decimal(Intensidade/Decimal(cos(angulo1)**2))
+                    I2 = Decimal(Intensidade*Decimal(cos(angulo2)**2))
 
-                I3 = I2*(cos(angulo3)**2)
-            else:
+                    I3 = Decimal(I2*Decimal(cos(angulo3)**2))
+                else:
 
-                I0 = Intensidade*2
-                I2 = Intensidade*(cos(angulo2)**2)
-                I3 = I2*(cos(angulo3)**2)
+                    I0 = Decimal(Intensidade*2)
+                    I2 = Decimal(Intensidade*Decimal(cos(angulo2)**2))
+                    I3 = Decimal(I2*Decimal(cos(angulo3)**2))
 
-                
+                print(f"""
+Resultados:
 
-            print(f"""
-I0 = {I0}
+I0 = {I0:.2e} W/cm²
 
-I2 = {I2}                  
+I2 = {I2:.2e} W/cm²                 
 
-I3 = {I3}
+I3 = {I3:.2e} W/cm²
 """)
-        case(3):
-            # Nesse caso entra os angulos a intensidade I2 e sai as instensidade I0, I1 e I3
-            Intensidade = float(input("Digite a intensidade de luz (I2): "))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-            angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
 
-            tipo = input("""
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))
+
+            case(3):
+                # Nesse caso entra os angulos a intensidade I2 e sai as instensidade I0, I1 e I3
+                Intensidade = Decimal(input("Digite a intensidade de luz (I2) W/cm²: "))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+                angulo3 = radians(float(input("Digite o angulo do terceiro polarizador (θ3°): ")))
+
+                tipo = input("""
 A luz inicial é polarizada?
                   
 s - Sim
 n - Não
 
 """)
-            
-            if tipo == "s":
-                I1 = Intensidade/(cos(angulo2)**2)
-                I0 = I1/(cos(angulo1)**2)
+                if tipo == "s":
+                    I1 = Decimal(Intensidade/Decimal(cos(angulo2)**2))
+                    I0 = Decimal(I1/Decimal(cos(angulo1)**2))
 
-                I3 = Intensidade*(cos(angulo3)**2)
-            else:
+                    I3 = Decimal(Intensidade*Decimal(cos(angulo3)**2))
+                else:
 
-                I1 = Intensidade/(cos(angulo2)**2)
-                I0 = I1*2
-                I3 = Intensidade*(cos(angulo3)**2)
+                    I1 = Decimal(Intensidade/Decimal(cos(angulo2)**2))
+                    I0 = Decimal(I1*2)
+                    I3 = Decimal(Intensidade*Decimal(cos(angulo3)**2))
+                print(f"""
+Resultados:
 
-                
+I0 = {I0:.2e} W/cm²
 
-            print(f"""
-I0 = {I0}
+I1 = {I1:.2e} W/cm²               
 
-I1 = {I1}                  
-
-I3 = {I3}
+I3 = {I3:.2e} W/cm²
 """)
-        case(4):
-            # Nesse caso entra os angulos a intensidade I3 e sai as instensidade I0, I1 e I2
-            Intensidade = float(input("Digite a intensidade de luz (I3): "))
-            angulo3 = radians(float(input("Digite o angulo do terceiro polarizador: ")))
-            angulo2 = radians(float(input("Digite o angulo do segundo polarizador: ")))
-            angulo1 = radians(float(input("Digite o angulo do primeiro polarizador: ")))
 
-            tipo = input("""
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))
+                
+            case(4):
+                # Nesse caso entra os angulos a intensidade I3 e sai as instensidade I0, I1 e I2
+                Intensidade = Decimal(input("Digite a intensidade de luz (I3) W/cm²: "))
+                angulo3 = radians(float(input("Digite o angulo do terceiro polarizador (θ3°): ")))
+                angulo2 = radians(float(input("Digite o angulo do segundo polarizador (θ2°): ")))
+                angulo1 = radians(float(input("Digite o angulo do primeiro polarizador (θ1°): ")))
+
+                tipo = input("""
 A luz inicial é polarizada?
                   
 s - Sim
 n - Não
 
 """)
-            
-            if tipo == "s":
-                I2 = Intensidade/(cos(angulo3)**2)
-                I1 = I2/(cos(angulo2)**2)
+                if tipo == "s":
+                    I2 = Decimal(Intensidade/Decimal(cos(angulo3)**2))
+                    I1 = Decimal(I2/Decimal(cos(angulo2)**2))
 
-                I0 = I1/(cos(angulo1)**2)
-            else:
+                    I0 = Decimal(I1/Decimal(cos(angulo1)**2))
+                else:
 
-                I2 = Intensidade/(cos(angulo3)**2)
+                    I2 = Decimal(Intensidade/Decimal(cos(angulo3)**2))
 
-                I1 = I2/(cos(angulo2)**2)
-                
-                I0 = I1*2
+                    I1 = Decimal(I2/Decimal(cos(angulo2)**2))
+                    
+                    I0 = Decimal(I1*2)
 
-            print(f"""
-I2 = {I2}                  
+                print(f"""
+Resultados:
 
-I0 = {I0}
+I0 = {I0:.2e} W/cm²
 
-I1 = {I1}
+I1 = {I1:.2e} W/cm²
+
+I2 = {I2:.2e} W/cm²                  
 """)
+
+                resposta = int(input("""
+Digite qual opção você gostaria de seguir:
+
+1 - Calculos com 2 polarizadores
+
+2 - Calculo com 3 polarizadores
+
+0 - Sair
+"""))     
